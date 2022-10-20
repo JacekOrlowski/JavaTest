cb746b42dee80de3619993130ac50f38ab9ceaf9
Jacek Orlowski <jacek.orlowski@asseco.pl>
2022-10-20 10:53:32 +0200

    Modyfikacja tekstu s.

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,4 +1,4 @@
 public class Test {
-    String s = "To jest tekst dodany w X.40";
+    String s = "To jest tekst dodany w X.40 modyfikacja w X.40";
     String s1= "To jest tekst dodane w X.41";
 }
